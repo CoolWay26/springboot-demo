52b250b12f3f719eb56d5946aaf2d68048f2f470
CoolWay26 <377905399@qq.com>
2021-04-15 18:59:59 +0800

    集成rabbitmq 简单模式测试rabbitmq

---
+++ src/main/java/com/coolway/controller/common/utils/RabbitMQUtil.java
@@ -0,0 +1,29 @@
+package com.coolway.controller.common.utils;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class RabbitMQUtil {
+    private final static String host = "192.168.31.95";
+    private final static int port = 5672;
+    private final static String username = "admin";
+    private final static String password = "123456";
+    private final static String virtualHost = "host.test1";
+
+    public static Connection getConnection() throws IOException, TimeoutException {
+        //创建连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        //设置RabbitMQ相关信息
+        factory.setHost(host);
+        factory.setPort(port);
+        factory.setUsername(username);
+        factory.setPassword(password);
+        factory.setVirtualHost(virtualHost);
+
+        //创建一个新的连接
+        return factory.newConnection();
+    }
+}
---
+++ src/main/java/com/coolway/controller/rabbitmq/simple/Customer.java
@@ -0,0 +1,43 @@
+package com.coolway.controller.rabbitmq.simple;
+
+import com.coolway.controller.common.utils.RabbitMQUtil;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Customer {
+    private final static String QUEUE_NAME = "queues.test1";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //创建一个新的连接
+        Connection connection = RabbitMQUtil.getConnection();
+        //创建一个频道
+        Channel channel = connection.createChannel();
+        //声明要关注的队列
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        //监听消息，设置消息处理
+        //DefaultConsumer类实现了Consumer接口，通过传入一个频道，告诉服务器我们需要哪个频道的消息
+        //如果频道中有消息，就会执行回调函数handleDelivery(..)，使用消息
+        //consumerTag：消费者标签，在channel.basicConsume(..)时指定
+        //envelope：消息包内容，包括消息ID（区别于消息者标签），routingkey，exchange，消息重转标记
+        //properties：消息属性
+        //body：消息
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("Customer Received '" + message + "'");
+            }
+        };
+        //自动回复队列应答 -- RabbitMQ中的消息确认机制
+        //第一个参数，指定要监听的队列
+        //第二个参数，是否自动回复，true表示接收到消息后自动通知mq收到了
+        //第三个参数，通过入参consumer执行回调函数handleDelivery(..)
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+
+        //消费者不会关闭资源，需要一直监听队列
+    }
+}
