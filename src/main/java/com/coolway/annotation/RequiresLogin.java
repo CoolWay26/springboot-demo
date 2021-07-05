eecf5f2d6dc792baee93dbb8c710454a89444b31
Steven/ZhuWei <377905399@qq.com>
2021-07-05 10:50:50 +0800

    JDK8可重复注解

--- src/main/java/com/coolway/annotation/RequiresLogin.java
+++ src/main/java/com/coolway/annotation/RequiresLogin.java
@@ -1,10 +1,8 @@
 package com.coolway.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Repeatable(RequiresLogins.class)
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RequiresLogin {
